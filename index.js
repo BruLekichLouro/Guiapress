2ded6563b709a84f74fc2575813a342e816a22f2
BruLekichLouro <bububio@yahoo.com.br>
2022-09-16 19:30:51 -0300

    Autenticando a conexão no index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 //View engine:
 app.set('view engine','ejs');
@@ -8,9 +9,18 @@ app.set('view engine','ejs');
 //Arquivos static:
 app.use(express.static('public'));
 //Body-parser (para aceitar dados de formulários):
-app.use(bodyParser.urlencoded({extends:false}));
+app.use(bodyParser.urlencoded({extend:false}));
 //aceitar também dados formato json:
 app.use(bodyParser.json());
+
+//Database:
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Conexão feita com sucesso")
+    }).catch((error)=>{
+        console.log(error);
+    });
 
 //Rota:
 app.get("/", (req, res) => {
