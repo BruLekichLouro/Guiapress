4d580e1a68704a72a904ae12176869f7cf11249c
BruLekichLouro <bububio@yahoo.com.br>
2022-09-16 17:27:57 -0300

    COnfigurando view engine

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+
+//View engine:
+app.set('view engine','ejs');
+
+//Rota:
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+//Servidor:
+app.listen(8080, ()=>{
+    console.log("Servidor está rodando!");
+})
