0930bbf3b6d4fc253f62969155ef7303e8a9a9d3
BruLekichLouro <bububio@yahoo.com.br>
2022-09-16 17:43:20 -0300

    Configurar body-parser para aceitar dados de formulários e de json; configurar express para usar arquivos estáticos

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //View engine:
 app.set('view engine','ejs');
+
+//Body-parser (paraaceitar dados de formulários):
+app.use(bodyParser.urlencoded({extends:false}));
+//aceitar também dados formato json:
+app.use(bodyParser.json());
 
 //Rota:
 app.get("/", (req, res) => {
