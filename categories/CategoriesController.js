40b36780f14c07bba19bcba609251f9a6621efed
BruLekichLouro <bububio@yahoo.com.br>
2022-09-22 12:23:44 -0300

    Adicionando rota para edição de categoria e view

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Category = require("./Category");
 const slugify = require("slugify");
 
-//Criando rota:
+//Rota:
 router.get("/admin/categories/new", (req, res)=>{
     res.render("admin/categories/new")
 });
@@ -12,12 +12,12 @@ router.get("/admin/categories/new", (req, res)=>{
 router.post("/categories/save", (req, res)=>{
     var title = req.body.title;
     if(title != undefined){//usuário não poderá cadastrar valor nulo
-
+        
         Category.create({
             title: title,
             slug: slugify(title)
         }).then(()=>{
-            res.redirect("/");
+            res.redirect("admin/categories");
         })
     }else{
         res.redirect("/admin/categories/new");
@@ -52,6 +52,25 @@ router.post("/categories/delete", (req, res)=>{
     }else{ //se for null
         res.redirect("/admin/categories")
     }
+});
+
+//Rota para edição da categoria:
+router.get("/admin/categories/edit/:id", (req, res) =>{
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    };
+    Category.findByPk(id).then(category =>{
+        if(category != undefined){
+            res.render("admin/categories/edit",{category: category})
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro=> {
+        res.redirect("/admin/categories");
+    })
 });
 
 module.exports = router;
