17530d5c3eff8afdf0d0d97350e5108fdfa40de9
BruLekichLouro <bububio@yahoo.com.br>
2022-09-22 18:28:29 -0300

    Iniciando o formulário de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,4 +5,9 @@ router.get("/articles", (req, res)=>{
     res.send("Rotas de artigos");
 });
 
+//Rota para criar novo artigo
+router.get("/admin/articles/new", (req, res) =>{
+    res.render("admin/articles/new");
+});
+
 module.exports = router;
